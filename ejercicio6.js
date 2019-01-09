45d1b4f694a43b25fdb86358109ffe2ff9ed2de3
ncristiansa <csalinasandia@iesesteveterradas.cat>
2019-01-09 20:27:09 +0100

    cambios 6 ejercicio

--- ejercicio6.js
+++ ejercicio6.js
@@ -2,11 +2,11 @@ var fs = require('fs')
 var path = require('path')
 
 module.exports = function(dir, filterStr, callback){
-	fs.readdir(dir, function (err list){
+	fs.readdir(dir, function (err,list){
 		if(err)
 			return callback(err)
 		list = list.filter(function (file){
 			return path.extname(file)=== '.' + filterStr
 })
-}
+})
 }
