fd908b3680ab81a7ce63118875ccb0d879d0971b
ncristiansa <csalinasandia@iesesteveterradas.cat>
2019-01-17 20:25:06 +0100

    cambios

---
+++ programa.js
@@ -0,0 +1,25 @@
+var http = require('http');
+/*
+http.createServer(function(req, res){
+    res.write('Hello world!');
+    res.end();
+}).listen(8080);
+*/
+
+//
+
+function especial(text, port, callback){
+    http.createServer(function(req, res){
+        res.write(text);
+        res.end();
+        callback(text);
+    }).listen(port);
+}
+especial('texto', 8080, especial2);
+var count = 0;
+function especial2(req){
+
+    console.log(req);
+    console.log(count);
+    count++;
+}
